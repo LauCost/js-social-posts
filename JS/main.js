04adf5f9c90ceb8ae46408e0a4706daae00ce76f
Laurentiu <lcostin40@gmail.com>
2021-10-25 16:37:08 +0200

    Creato array di oggetti

---
+++ JS/main.js
@@ -0,0 +1,32 @@
+
+const post = [
+    {
+        id: 1,
+
+        contenuto: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia quos maxime praesentium consequatur vitae odit quod vero? Placeat consequatur iusto, reiciendis, assumenda quibusdam doloremque eius repudiandae ducimus quis beatae eum.`,
+
+        immagine: "https://i.picsum.photos/id/235/300/150.jpg?hmac=pqOChsrlOT9osSgCbBO6w-hUXedcHMMHrCmOsZK183E",
+
+        autore: "Phil Mangione"
+    },
+
+    {
+        id: 2,
+
+        contenuto: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia quos maxime praesentium consequatur vitae odit quod vero? Placeat consequatur iusto, reiciendis, assumenda quibusdam doloremque eius repudiandae ducimus quis beatae eum.`,
+
+        immagine: "https://i.picsum.photos/id/40/300/150.jpg?hmac=isDdgEQixAVlpGn5X68Md6TASzthjtAVxvKN0asYeA4",
+
+        autore: "John Wick"
+    },
+
+    {
+        id: 3,
+
+        contenuto: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia quos maxime praesentium consequatur vitae odit quod vero? Placeat consequatur iusto, reiciendis, assumenda quibusdam doloremque eius repudiandae ducimus quis beatae eum.`,
+
+        immagine: "https://i.picsum.photos/id/796/300/150.jpg?hmac=u3RRUqpGvjaHMsiyS-v0sW4uZMi5R_L6wHckvxE379w",
+
+        autore: "Giulio Cesare"
+    },
+]
