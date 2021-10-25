97de3ad1186fd8d1c3444379a56bbcef991a17f3
Laurentiu <lcostin40@gmail.com>
2021-10-25 16:56:59 +0200

    Aggiunto iniziali del nome utente all'immagine profilo

--- JS/main.js
+++ JS/main.js
@@ -3,6 +3,8 @@ const post = [
     {
         id: 1,
 
+        nomeImg: "PM",
+
         contenuto: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia quos maxime praesentium consequatur vitae odit quod vero? Placeat consequatur iusto, reiciendis, assumenda quibusdam doloremque eius repudiandae ducimus quis beatae eum.`,
 
         immagine: "https://i.picsum.photos/id/235/300/150.jpg?hmac=pqOChsrlOT9osSgCbBO6w-hUXedcHMMHrCmOsZK183E",
@@ -11,11 +13,13 @@ const post = [
 
         numeroLike: 80,
 
-        dataCreazione: "4 mesi fa"
+        dataCreazione: "2 mesi fa"
     },
 
     {
         id: 2,
+
+        nomeImg: "JW",
 
         contenuto: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia quos maxime praesentium consequatur vitae odit quod vero? Placeat consequatur iusto, reiciendis, assumenda quibusdam doloremque eius repudiandae ducimus quis beatae eum.`,
 
@@ -31,6 +35,8 @@ const post = [
     {
         id: 3,
 
+        nomeImg: "GC",
+
         contenuto: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia quos maxime praesentium consequatur vitae odit quod vero? Placeat consequatur iusto, reiciendis, assumenda quibusdam doloremque eius repudiandae ducimus quis beatae eum.`,
 
         immagine: "https://i.picsum.photos/id/796/300/150.jpg?hmac=u3RRUqpGvjaHMsiyS-v0sW4uZMi5R_L6wHckvxE379w",
@@ -39,7 +45,7 @@ const post = [
 
         numeroLike: 90,
 
-        dataCreazione: "2 mesi fa"
+        dataCreazione: "4 mesi fa"
     },
 ]
 
@@ -52,7 +58,7 @@ for (i = 0; i < post.length; i++) {
     <div class="post">
             <div class="utente">
                 <span class="img_utente">
-
+                ${socialPost.nomeImg}
                 </span>
                 <p class="nome_tempo">
                     <span class="nome">
