28c99caf21e4eec0655c75e318677abe26f780dc
Laurentiu <lcostin40@gmail.com>
2021-10-25 16:49:59 +0200

    Creato codice per aggiungere i vari post che sono nell'array di oggetti

--- JS/main.js
+++ JS/main.js
@@ -7,7 +7,11 @@ const post = [
 
         immagine: "https://i.picsum.photos/id/235/300/150.jpg?hmac=pqOChsrlOT9osSgCbBO6w-hUXedcHMMHrCmOsZK183E",
 
-        autore: "Phil Mangione"
+        autore: "Phil Mangione",
+
+        numeroLike: 80,
+
+        dataCreazione: "4 mesi fa"
     },
 
     {
@@ -17,7 +21,11 @@ const post = [
 
         immagine: "https://i.picsum.photos/id/40/300/150.jpg?hmac=isDdgEQixAVlpGn5X68Md6TASzthjtAVxvKN0asYeA4",
 
-        autore: "John Wick"
+        autore: "John Wick",
+
+        numeroLike: 180,
+
+        dataCreazione: "3 mesi fa"
     },
 
     {
@@ -27,6 +35,56 @@ const post = [
 
         immagine: "https://i.picsum.photos/id/796/300/150.jpg?hmac=u3RRUqpGvjaHMsiyS-v0sW4uZMi5R_L6wHckvxE379w",
 
-        autore: "Giulio Cesare"
+        autore: "Giulio Cesare",
+
+        numeroLike: 90,
+
+        dataCreazione: "2 mesi fa"
     },
 ]
+
+
+for (i = 0; i < post.length; i++) {
+
+    const socialPost = post[i]
+
+    const elemento = `
+    <div class="post">
+            <div class="utente">
+                <span class="img_utente">
+
+                </span>
+                <p class="nome_tempo">
+                    <span class="nome">
+                        ${socialPost.autore}
+                    </span> <br>
+                    ${socialPost.dataCreazione}
+                </p>
+            </div>
+
+            <div class="testo">
+                <p>
+                    ${socialPost.contenuto}
+                </p>
+            </div>
+
+            <div class="img">
+                <img src="${socialPost.immagine}"
+                    alt="img">
+            </div>
+
+            <div class="like">
+                <a href="#">
+                    <i class="fas fa-thumbs-up"></i> Mi piace
+                </a>
+
+                <p class="piace">
+                    Piace a <span class="numero"> ${socialPost.numeroLike} </span> persone
+                </p>
+            </div>
+    `
+
+    const container = document.querySelector(".container")
+
+    container.innerHTML += elemento
+}
